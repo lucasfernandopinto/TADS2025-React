96c2285b9af05a9310738ebc2912921198933d8c
Lucas Fernando Pinto <40510593+lucasfernandopinto@users.noreply.github.com>
2025-07-12 20:49:24 -0300

    Criação do projeto
    Criação do projeto com o vite e o typescript e Exercicio 1.

---
+++ 2BATV01/meu-primeiro-app-react/src/App.tsx
@@ -0,0 +1,18 @@
+// importa o React da biblioteca 'react'.
+import React from 'react';
+
+// cria um componente chamado 'App'.
+function App() {
+  // contém o que será renderizado na tela.
+  return ( 
+    <> 
+      {
+       // exibe o texto "Meu primeiro app React" na página.
+      }
+      <h1>Meu primeiro app React</h1>
+    </>
+  );
+}
+
+// exporta o componente 'App' como o export padrão deste módulo.
+export default App;
