eb7bb6997692292f1263a33677935c60d6698cfb
Lucas Fernando Pinto <40510593+lucasfernandopinto@users.noreply.github.com>
2025-07-12 20:52:16 -0300

    Exerrcicio 02

--- 2BATV01/meu-primeiro-app-react/src/App.tsx
+++ 2BATV01/meu-primeiro-app-react/src/App.tsx
@@ -3,6 +3,11 @@ import React from 'react';
 
 // cria um componente chamado 'App'.
 function App() {
+  // declaração da variável 'nome' com o valor "Ana".
+  const nome = "Ana";
+  // declaração da variável 'anoNascimento' com o valor 2005.
+  const anoNascimento = 2005;
+
   // contém o que será renderizado na tela.
   return ( 
     <> 
@@ -10,6 +15,11 @@ function App() {
        // exibe o texto "Meu primeiro app React" na página.
       }
       <h1>Meu primeiro app React</h1>
+      {
+      // define um paragrafo <p> que escreve "Olá, meu nome é Ana e nasci em 2005"
+      <p>Olá, meu nome é {nome} e nasci em {anoNascimento}.</p>
+      }
+
     </>
   );
 }
